62f4ed90d5a8be6d937297c42df64e812023e0a1
LipeClash <felipeclaudino2003@hotmail.com>
2020-06-05 07:50:19 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CalculadoraIdade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Minha idade em dias:");
+            Console.WriteLine();
+
+            DateTime data1, data2, data3;
+            TimeSpan result;
+
+            data1 = DateTime.Parse("30/01/2003").Date;
+            data2 = DateTime.Parse("30/01/2003").Date;
+            data3 = DateTime.Parse("05/06/2020").Date;
+
+            result = data3 - data2;
+
+            Console.WriteLine($"Eu nasci em {data1} e já vivi {result.Days} dias.");
+
+            
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CalculadoraIdade
 05/06/2020
+
+feito
